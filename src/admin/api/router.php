535b4679e2fba38a07032e5107dd886b7c2ed749
ShehabMuthannH <your.email@example.com>
2026-05-10 20:15:17 +0300

    Remove router.php to let tests generate it

--- src/admin/api/router.php
+++
@@ -1,6 +0,0 @@
-<?php
-chdir(__DIR__);
-require_once __DIR__ . '/db.php';
-$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-if ($uri !== '/' && $uri !== '/index.php') { return false; }
-require __DIR__ . '/index.php';
